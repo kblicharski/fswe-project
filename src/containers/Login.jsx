273ff17f7c3e8bd9406327efffd35fe4b1b4e879
Kevin Blicharski <blicharskikevin@gmail.com>
2018-03-05 15:44:35 -0600

    feat(f): implement login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -14,13 +14,13 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
+      userName: '',
       password: ''
     };
   }
 
   validateForm() {
-    return this.state.username.length > 0 && this.state.password.length > 0;
+    return this.state.userName.length > 0 && this.state.password.length > 0;
   }
 
   handleChange = (event) => {
@@ -31,22 +31,42 @@ export default class Login extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
+    let valid = false;
     const instance = axios.create({baseURL: 'http://localhost:3000'});
     instance.get('/api/voters')
-      .then((response) => {console.log(response);})
-      .catch((error) => {console.log(error);});
+      .then(
+        (response) => {
+          for (const elem in response.data) {
+            const user = response.data[elem];
+            if (user.password === this.state.password &&
+              user.userName === this.state.userName) {
+              valid = true;
+            }
+          }
+          if (valid) {
+            this.props.history.push('/home');
+          } else {
+            console.log('Invalid username or password.');
+          }
+        }
+      )
+      .catch(
+        (error) => {
+          console.log(error);
+        }
+      );
   };
 
   render() {
     return (
       <div className="Login">
         <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="username" bsSize="large">
+          <FormGroup controlId="userName" bsSize="large">
             <ControlLabel>Username</ControlLabel>
             <FormControl
               autoFocus
               type="username"
-              value={this.state.username}
+              value={this.state.userName}
               onChange={this.handleChange}
             />
           </FormGroup>
