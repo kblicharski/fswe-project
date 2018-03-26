628e9da7ea262c0ca7bc4d7d2a79aee31f5a0239
Joseph Leiferman <joseph.leiferman@gmail.com>
2018-03-26 15:09:02 -0500

    feat(f): data validation, add voting endpoints

--- common/models/manager.js
+++ common/models/manager.js
@@ -1,5 +1,10 @@
 'use strict';
 
-module.exports = function(Manager) {
+//contains one lower-case, one upper-case, one special character, and minimum of 6 characters
+var strongPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})");
 
+module.exports = function(Manager) {
+  Manager.validatesLengthOf('password', {min: 6, message: {min: 'Password is too short'}});
+  Manager.validatesFormatOf('password', {with: strongPassword, message : {with: 'password is in wrong format'}});
+  Manager.validatesFormatOf('userName', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
 };
