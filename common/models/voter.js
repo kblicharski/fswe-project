3ec1e206644064e90dcfc6f2d7dee8833affac1d
Joseph Leiferman <joseph.leiferman@gmail.com>
2018-04-02 14:53:33 -0500

    fix: missing relation that caused error when calling 'voters/login'

--- common/models/voter.js
+++ common/models/voter.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 //contains one lower-case, one upper-case, one special character, and minimum of 6 characters
 
 var strongPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})");
@@ -8,7 +9,7 @@ module.exports = function(User) {
   User.validatesLengthOf('password', {min: 6, message: {min: 'Password is too short'}});
   User.validatesFormatOf('password', {with: strongPassword , message : {with: 'Password is in wrong format'}});
   User.validatesFormatOf('email', {with: /\S+@\S+\.\S+/, message : {with: 'Email is in wrong format'}});
-  User.validatesFormatOf('userName', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
+  User.validatesFormatOf('username', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
   User.validatesLengthOf('ssn', {min: 5, max: 5, message: {min: 'SSN is too short', max: 'SSN is too long'}});
   User.validate('status', statusValidator, {message: 'Invalid status'});
 };
@@ -16,3 +17,4 @@ module.exports = function(User) {
 function statusValidator(err) {
   if (this.status !== ('unregistered' || 'registered')) err();
 };
+
--- server/boot/create-sample-model.js
+++ server/boot/create-sample-model.js
@@ -1,19 +1,21 @@
 module.exports = function(app) {
-  app.dataSources.mysqlDS.automigrate('user', function(err) {
+  app.dataSources.mysqlDS.automigrate('voter', function(err) {
     if (err) throw err;
 
-    app.models.user.create([{
-      email: "jon.smith@nope.com",
-      userName: "jonSmith",
-      password: "joN123@",
+    app.models.voter.create([{
+      email: "joseph.leiferman@gmail.com",
+      username: "jonSmith",
+      password: "jonSmith!",
       ssn: "55555",
       dob: "2018-02-24T23:40:26.663Z",
       driversLicense: "js555123",
-      status: "unregistered"
-    }], function(err, users) {
+      status: "unregistered",
+      emailVerified: true
+
+    }], function(err, voters) {
       if (err) throw err;
 
-      console.log('User models created: \n', users);
+      console.log('Voter models created: \n', voters);
     });
   });
   app.dataSources.mysqlDS.automigrate('administrators', function(err) {
