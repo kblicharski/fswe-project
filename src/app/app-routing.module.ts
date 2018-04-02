806b845a395788b215fb7776febe1e2f008945f5
Kevin Blicharski <blicharskikevin@gmail.com>
2018-04-02 04:13:23 -0500

    feat: add to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { NgModule } from '@angular/core';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'password-reset', component: PasswordResetComponent},
   {path: '**', redirectTo: ''}
 ];
 
