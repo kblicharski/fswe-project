d28427c87148f9ba250d981f5344d325b37a83cb
Joseph Leiferman <joseph.leiferman@gmail.com>
2018-04-02 15:22:36 -0500

    fix: typos in voter.js & routes.js

--- common/models/voter.js
+++ common/models/voter.js
@@ -5,13 +5,13 @@
 
 var strongPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{6,})");
 
-module.exports = function(User) {
-  User.validatesLengthOf('password', {min: 6, message: {min: 'Password is too short'}});
-  User.validatesFormatOf('password', {with: strongPassword , message : {with: 'Password is in wrong format'}});
-  User.validatesFormatOf('email', {with: /\S+@\S+\.\S+/, message : {with: 'Email is in wrong format'}});
-  User.validatesFormatOf('username', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
-  User.validatesLengthOf('ssn', {min: 5, max: 5, message: {min: 'SSN is too short', max: 'SSN is too long'}});
-  User.validate('status', statusValidator, {message: 'Invalid status'});
+module.exports = function(Voter) {
+  Voter.validatesLengthOf('password', {min: 6, message: {min: 'Password is too short'}});
+  Voter.validatesFormatOf('password', {with: strongPassword , message : {with: 'Password is in wrong format'}});
+  Voter.validatesFormatOf('email', {with: /\S+@\S+\.\S+/, message : {with: 'Email is in wrong format'}});
+  Voter.validatesFormatOf('username', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
+  Voter.validatesLengthOf('ssn', {min: 5, max: 5, message: {min: 'SSN is too short', max: 'SSN is too long'}});
+  Voter.validate('status', statusValidator, {message: 'Invalid status'});
 };
 
 function statusValidator(err) {
--- server/boot/routes.js
+++ server/boot/routes.js
@@ -2,7 +2,7 @@ var dsConfig = require('../datasources.json');
 var path = require('path');
 
 module.exports = function(app) {
-  var User = app.models.user;
+  var Voter = app.models.voter;
 
   //login page
   app.get('/', function(req, res) {
@@ -20,7 +20,7 @@ module.exports = function(app) {
 
   //log a user in
   app.post('/login', function(req, res) {
-    User.login({
+    Voter.login({
       username: req.body.username,
       password: req.body.password
     }, 'user', function(err, token) {
@@ -55,7 +55,7 @@ module.exports = function(app) {
   //log a user out
   app.get('/logout', function(req, res, next) {
     if (!req.accessToken) return res.sendStatus(401);
-    User.logout(req.accessToken.id, function(err) {
+    Voter.logout(req.accessToken.id, function(err) {
       if (err) return next(err);
       res.redirect('/');
     });
@@ -63,7 +63,7 @@ module.exports = function(app) {
 
   //send an email with instructions to reset an existing user's password
   app.post('/request-password-reset', function(req, res, next) {
-    User.resetPassword({
+    Voter.resetPassword({
       email: req.body.email
     }, function(err) {
       if (err) return res.status(401).send(err);
