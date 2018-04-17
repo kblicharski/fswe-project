d6aa78473af974fdeb17990c8626a71804222892
Joseph Leiferman <joseph.leiferman@gmail.com>
2018-04-17 11:09:36 -0500

    fix: userName -> username

--- common/models/administrators.js
+++ common/models/administrators.js
@@ -6,5 +6,5 @@ var strongPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\
 module.exports = function(Administrators) {
   Administrators.validatesLengthOf('password', {min: 6, message: {min: 'Password is too short'}});
   Administrators.validatesFormatOf('password', {with: strongPassword, message : {with: 'password is in wrong format'}});
-  Administrators.validatesFormatOf('userName', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
+  Administrators.validatesFormatOf('username', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
 };
--- common/models/manager.js
+++ common/models/manager.js
@@ -6,5 +6,5 @@ var strongPassword = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\
 module.exports = function(Manager) {
   Manager.validatesLengthOf('password', {min: 6, message: {min: 'Password is too short'}});
   Manager.validatesFormatOf('password', {with: strongPassword, message : {with: 'password is in wrong format'}});
-  Manager.validatesFormatOf('userName', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
+  Manager.validatesFormatOf('username', {with: /^[a-zA-Z0-9]+$/, message : {with: 'User name is in wrong format'}});
 };
