75749d54de585655e41c274eccfd5290c350e03c
Joseph Leiferman <joseph.leiferman@gmail.com>
2018-04-20 15:20:56 -0500

    FIX: merge admin, voter, manager model. Create ballot model

--- server/boot/create-sample-model.js
+++ server/boot/create-sample-model.js
@@ -1,9 +1,10 @@
 module.exports = function(app) {
-  app.dataSources.mysqlDS.automigrate('voter', function(err) {
+  app.dataSources.mysqlDS.automigrate('user', function(err) {
     if (err) throw err;
 
-    app.models.voter.create([{
-      email: "joseph.leiferman@gmail.com",
+    app.models.user.create([
+      {
+      email: "jon.smith@gmail.com",
       username: "jonSmith",
       password: "jonSmith!",
       ssn: "55555",
@@ -11,41 +12,49 @@ module.exports = function(app) {
       driversLicense: "js555123",
       status: "unregistered",
       emailVerified: true,
+      role: "voter",
       address: {
         street: "University Avenue",
         city: "Iowa City",
         state: "IA",
         zipCode: "52245"
-      },
-
+      }
+    },{
+      email: "jon.Adams@gmail.com",
+      username: "jonAdams",
+      password: "jonAdams!",
+      ssn: "66666",
+      dob: "2018-02-25T23:40:26.663Z",
+      driversLicense: "ja555123",
+      status: "registered",
+      emailVerified: true,
+      role: "administrator",
+      address: {
+        street: "University Avenue",
+        city: "Iowa City",
+        state: "IA",
+        zipCode: "52245"
+      }
+      },{
+        email: "jon.Clancy@gmail.com",
+        username: "jonClancy",
+        password: "jonClancy!",
+        ssn: "77777",
+        dob: "2018-02-26T23:40:26.663Z",
+        driversLicense: "jc555123",
+        status: "registered",
+        emailVerified: true,
+        role: "manager",
+        address: {
+          street: "University Avenue",
+          city: "Iowa City",
+          state: "IA",
+          zipCode: "52245"
+        }
     }], function(err, voters) {
       if (err) throw err;
 
-      console.log('Voter models created: \n', voters);
-    });
-  });
-  app.dataSources.mysqlDS.automigrate('administrators', function(err) {
-    if (err) throw err;
-
-    app.models.administrators.create([{
-      username: "jDeer",
-      password: "jD123@"
-    }], function(err, administrators) {
-      if (err) throw err;
-
-      console.log('Administrator models created: \n', administrators);
-    });
-  });
-  app.dataSources.mysqlDS.automigrate('manager', function(err) {
-    if (err) throw err;
-
-    app.models.manager.create([{
-      username: "jDoe",
-      password: "jDoe123@"
-    }], function(err, manager) {
-      if (err) throw err;
-
-      console.log('Manager models created: \n', manager);
+      console.log('User models created: \n', voters);
     });
   });
   app.dataSources.mysqlDS.automigrate('vote', function(err) {
@@ -53,16 +62,31 @@ module.exports = function(app) {
 
     app.models.vote.create([{
       ballotNumber: 1,
-      candidate:  ["John Adams", "John Smith"],
+      candidate:  "John Adams",
       voter: 1,
-      location: "Iowa City",
-      timeStart: new Date(2018,5,20),
-      timeEnd: new Date(2018,6,20)
+      time: new Date(2018,5,20)
 
     }], function(err, vote) {
       if (err) throw err;
 
       console.log('Vote models created: \n', vote);
+    });
+  });
+  app.dataSources.mysqlDS.automigrate('ballot', function(err) {
+    if (err) throw err;
+
+    app.models.ballot.create([{
+      ballotNumber: 1,
+      candidates:  ["John Adams", "John Smith"],
+      managers: ["Suzy Collins"],
+      locations: ["Iowa City"],
+      start: new Date(2018,5,20),
+      end: new Date(2018,6,20)
+
+  }], function(err, vote) {
+      if (err) throw err;
+
+      console.log('Ballot models created: \n', vote);
     });
   });
   app.dataSources.mysqlDS.automigrate('accessToken', function(err) {
