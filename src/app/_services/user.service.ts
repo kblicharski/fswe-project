102e55d08e13668468893ab223c965381c2aff6e
Kevin Blicharski <blicharskikevin@gmail.com>
2018-04-23 19:25:31 -0500

    hook up users to db

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -2,31 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { User } from '../_models/user';
-import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class UserService {
+  private apiUrl = 'http://localhost:3000/api';
+
   constructor(private http: HttpClient) {
   }
 
   getAll() {
-    // return this.http.get<User[]>('/api/users');
-    return of(JSON.parse(localStorage.getItem('users')));
+    const url = `${this.apiUrl}/users`;
+    return this.http.get<User[]>(url);
   }
 
   getById(id: number) {
-    return this.http.get('/api/users/' + id);
+    const url = `${this.apiUrl}/users/${id}`;
+    return this.http.get(url);
   }
 
   create(user: User) {
-    return this.http.post('/api/users', user);
+    const url = `${this.apiUrl}/users`;
+    return this.http.post(url, user);
   }
 
   update(user: User) {
-    return this.http.put('/api/users/' + user.id, user);
+    const url = `${this.apiUrl}/users/${user.id}`;
+    return this.http.patch(url, user);
   }
 
   delete(id: number) {
-    return this.http.delete('/api/users/' + id);
+    const url = `${this.apiUrl}/users/${id}`;
+    return this.http.delete(url);
   }
+
 }
