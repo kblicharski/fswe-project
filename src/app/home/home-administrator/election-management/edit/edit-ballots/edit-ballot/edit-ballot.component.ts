a3adbcb68fc811445b4f222ced0782fc15d91379
Kevin Blicharski <blicharskikevin@gmail.com>
2018-04-30 13:34:10 -0500

    make shells

--- src/app/home/home-administrator/election-management/edit/edit-ballots/edit-ballot/edit-ballot.component.ts
+++ src/app/home/home-administrator/election-management/edit/edit-ballots/edit-ballot/edit-ballot.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuditService } from '../../../../../../_services/audit.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Office } from '../../../../../../_models/office';
+import { ElectionService } from '../../../../../../_services/election.service';
 
 @Component({
   selector: 'app-edit-ballot',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-ballot.component.css']
 })
 export class EditBallotComponent implements OnInit {
+  ballot: Office;
+  loading = true;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private electionService: ElectionService,
+    private auditService: AuditService,
+  ) {
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.electionService.getOfficeById(id).subscribe(
+        (data) => {
+          this.ballot = data;
+          this.loading = false;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    });
+  }
+
+  onUpdateBallot() {
+    const old = this.ballot;
+    this.electionService.updateOffice(this.ballot).subscribe(
+      (data) => {
+        const audit = {
+          action: `Ballot ${old.id} was updated`,
+          time: new Date(Date.now())
+        };
+        this.auditService.logAudit(audit).subscribe(
+          (data) => {
+            this.router.navigate(['..'], {relativeTo: this.route});
+          }
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
 }
--- src/app/home/home-administrator/election-management/edit/edit-candidates/edit-candidate/edit-candidate.component.ts
+++ src/app/home/home-administrator/election-management/edit/edit-candidates/edit-candidate/edit-candidate.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuditService } from '../../../../../../_services/audit.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ElectionService } from '../../../../../../_services/election.service';
+import { Candidate } from '../../../../../../_models/candidate';
 
 @Component({
   selector: 'app-edit-candidate',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-candidate.component.css']
 })
 export class EditCandidateComponent implements OnInit {
+  candidate: Candidate;
+  loading = true;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private electionService: ElectionService,
+    private auditService: AuditService,
+  ) {
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+      this.electionService.getCandidateById(id).subscribe(
+        (data) => {
+          this.candidate = data;
+          this.loading = false;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    });
+  }
+
+  onUpdateCandidate() {
+    const old = this.candidate;
+    this.electionService.updateCandidate(this.candidate).subscribe(
+      (data) => {
+        const audit = {
+          action: `Candidate ${old.id} was updated`,
+          time: new Date(Date.now())
+        };
+        this.auditService.logAudit(audit).subscribe(
+          (data) => {
+            this.router.navigate(['..'], {relativeTo: this.route});
+          }
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
 }
