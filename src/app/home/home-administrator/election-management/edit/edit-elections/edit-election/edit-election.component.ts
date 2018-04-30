615ae0fa09b9eb840e8549ec83595b8840dd13fa
Kevin Blicharski <blicharskikevin@gmail.com>
2018-04-30 17:02:37 -0500

    fix edit

--- src/app/home/home-administrator/election-management/edit/edit-elections/edit-election/edit-election.component.ts
+++ src/app/home/home-administrator/election-management/edit/edit-elections/edit-election/edit-election.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuditService } from '../../../../../../_services/audit.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Election } from '../../../../../../_models/election';
 import { ElectionService } from '../../../../../../_services/election.service';
+import { Office } from '../../../../../../_models/office';
 
 @Component({
   selector: 'app-edit-election',
@@ -10,8 +10,16 @@ import { ElectionService } from '../../../../../../_services/election.service';
   styleUrls: ['./edit-election.component.css']
 })
 export class EditElectionComponent implements OnInit {
-  election: Election;
+  election: any = {};
   loading = true;
+
+  ballots = [];
+  managers = [];
+  precincts = [];
+
+  selectedBallots = [];
+  selectedManagers = [];
+  selectedPrecincts = [];
 
   constructor(
     private router: Router,
@@ -27,7 +35,37 @@ export class EditElectionComponent implements OnInit {
       this.electionService.getById(id).subscribe(
         (data) => {
           this.election = data;
-          this.loading = false;
+          this.electionService.getAllOffices().subscribe(
+            (data) => {
+              this.ballots = data;
+              console.log('offices', data);
+
+              this.electionService.getAllManagers().subscribe(
+                (data) => {
+                  this.managers = data;
+                  console.log('managers', data);
+
+                  this.electionService.getPrecincts().subscribe(
+                    (data) => {
+                      this.precincts = data;
+                      console.log('precincts', data);
+                      this.loading = false;
+                    },
+                    (error) => {
+                      console.log(error);
+                    }
+                  );
+
+                },
+                (error) => {
+                  console.log(error);
+                }
+              );
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
         },
         (error) => {
           console.log(error);
@@ -38,6 +76,27 @@ export class EditElectionComponent implements OnInit {
 
   onUpdateElection() {
     const old = this.election;
+
+    const precinctIds = [];
+    const managerIds = [];
+    const ballotIds = [];
+
+    for (const val of this.selectedBallots) {
+      ballotIds.push(val.id);
+    }
+
+    for (const val of this.selectedManagers) {
+      managerIds.push(val.id);
+    }
+
+    for (const val of this.selectedPrecincts) {
+      precinctIds.push(val.id);
+    }
+
+    this.election.offices = ballotIds;
+    this.election.precincts = precinctIds;
+    this.election.managers = managerIds;
+
     this.electionService.update(this.election).subscribe(
       (data) => {
         const audit = {
@@ -54,6 +113,42 @@ export class EditElectionComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+
+  changePrecincts($event, precinct: any) {
+    if ($event.target.checked) {
+      this.selectedPrecincts.push(precinct);
+    } else {
+      const updateItem = this.selectedPrecincts.find(this.findIndexToUpdate, precinct.id);
+      const index = this.selectedPrecincts.indexOf(updateItem);
+      this.selectedPrecincts.splice(index, 1);
+    }
+  }
+
+
+  changeBallots($event, ballot: Office) {
+    if ($event.target.checked) {
+      this.selectedBallots.push(ballot);
+    } else {
+      const updateItem = this.selectedBallots.find(this.findIndexToUpdate, ballot.id);
+      const index = this.selectedBallots.indexOf(updateItem);
+      this.selectedBallots.splice(index, 1);
+    }
+  }
+
+  changeManagers($event, manager: any) {
+    if ($event.target.checked) {
+      this.selectedManagers.push(manager);
+    } else {
+      const updateItem = this.selectedManagers.find(this.findIndexToUpdate, manager.id);
+      const index = this.selectedManagers.indexOf(updateItem);
+      this.selectedManagers.splice(index, 1);
+    }
+  }
+
+  findIndexToUpdate(val) {
+    return val.id === this;
   }
 
 }
